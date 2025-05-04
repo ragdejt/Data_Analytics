bd97c97ee56221bee0367b917a9b1fdb7d27a04b
r4gd3j7 <rogerio210400@gmail.com>
2025-05-04 13:01:47 -0300

    .

--- Data_Analytics.py
+++ Data_Analytics.py
@@ -367,8 +367,6 @@ if "login" in streamlit.session_state and streamlit.session_state["login"]:
                 streamlit.info("Útil para entender a complexidade por categoria (Ultra congelado, Congelado, Resfriado, Seco")
                     
 else:
-    print("Starting Application! [...]")
-    print("Searching for files needed to run application! [...]")
     SCRIPT_FOLDER.mkdir(exist_ok=True)
     SQL_FOLDER.mkdir(exist_ok=True)
     EXCEL_FOLDER.mkdir(exist_ok=True)
