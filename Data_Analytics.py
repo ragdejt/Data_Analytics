68385be4ead1fa52aec7223c17984df332f78450
r4gd3j7 <rogerio.moreira210400@gmail.com>
2025-05-04 15:22:40 +0000

    .

--- Data_Analytics.py
+++ Data_Analytics.py
@@ -367,6 +367,8 @@ if "login" in streamlit.session_state and streamlit.session_state["login"]:
                 streamlit.info("Útil para entender a complexidade por categoria (Ultra congelado, Congelado, Resfriado, Seco")
                     
 else:  
+    
+
     SCRIPT_FOLDER.mkdir(exist_ok=True)
     SQL_FOLDER.mkdir(exist_ok=True)
     EXCEL_FOLDER.mkdir(exist_ok=True)
